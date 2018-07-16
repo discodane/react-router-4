9fbf396e5d55eb43376dc227a4826e59ac5541eb
discodane <el.dane.erickson@gmail.com>
2018-07-16 10:06:43 -0500

    Initial routes and config created

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+//App Components
+import Home from './Home';
+import About from './About';
+import Header from './Header';
+import Courses from './Courses';
+import Teachers from './Teachers';
+
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/courses" component={Courses} />
+      <Route path="/teachers" component={Teachers} />
+    </div>
+  </BrowserRouter>
+);
+
+export default App;
